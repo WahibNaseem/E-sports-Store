1015e777155420ef4418dd92aeb2b6514177aaea
Wahib <wahib1900@gmail.com>
2022-12-07 10:30:34 +0800

    added brands and types entity with API methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,16 +15,18 @@ namespace API.Controllers
   public class ProductsController : ControllerBase
   {
     private readonly StoreContext _context;
-    public ProductsController(StoreContext context)
+    private readonly IProductRepository _repo;
+    public ProductsController(StoreContext context, IProductRepository repo)
     {
+      _repo = repo;
       _context = context;
 
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> Products()
+    public async Task<ActionResult<IReadOnlyList<Product>>> Products()
     {
-      var products = await _context.Products.ToListAsync();
+      var products = await _repo.GetProductsAsync();
       return Ok(products);
     }
 
@@ -33,8 +36,22 @@ namespace API.Controllers
       if (id <= 0)
         return BadRequest("Id can not be zero or less then zero");
 
-      var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+      var product = await _repo.GetProductByIdAsync(id);
       return Ok(product);
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrandsAsync()
+    {
+      var productBrands = await _repo.GetProductBrandsAsync();
+      return Ok(productBrands);
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+      var productTypes = await _repo.GetProductTypes();
+      return Ok(productTypes);
     }
   }
 }
